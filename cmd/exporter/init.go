ad9b21b9ec9b4409a9c24922ff0201c5acc1b665
jizhong.jiangjz <jizhong.jiangjz@alibaba-inc.com>
2023-09-28 17:13:24 +0800

    import flow package in init.go to enable flow probe

--- cmd/exporter/init.go
+++ cmd/exporter/init.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "github.com/alibaba/kubeskoop/pkg/exporter/probe/flow"
 	_ "github.com/alibaba/kubeskoop/pkg/exporter/probe/nlconntrack"
 	_ "github.com/alibaba/kubeskoop/pkg/exporter/probe/nlqdisc"
 	_ "github.com/alibaba/kubeskoop/pkg/exporter/probe/procfd"
