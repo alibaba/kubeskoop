2480a1162141a8ff1b721d9700f16b1ca751c9cb
jizhong.jiangjz <jizhong.jiangjz@alibaba-inc.com>
2024-01-08 16:11:24 +0800

    remove unused field in sandboxInfoSpec

--- pkg/exporter/nettop/pod.go
+++ pkg/exporter/nettop/pod.go
@@ -1,22 +1,22 @@
 package nettop
 
 // from containerd sandbox status
-type NamespacesSpec struct {
-	Type string `json:"type"`
-	Path string `json:"path"`
-}
+//type NamespacesSpec struct {
+//	Type string `json:"type"`
+//	Path string `json:"path"`
+//}
 
-type LinuxSpec struct {
-	NamespaceSpec []NamespacesSpec `json:"namespaces"`
-}
+//type LinuxSpec struct {
+//	NamespaceSpec []NamespacesSpec `json:"namespaces"`
+//}
 
-type RuntimeSpec struct {
-	Linux LinuxSpec `json:"linux"`
-}
+//type RuntimeSpec struct {
+//	Linux LinuxSpec `json:"linux"`
+//}
 
-type RuntimeOptions struct {
-	SystemdCGroup bool `json:"systemd_cgroup"`
-}
+//type RuntimeOptions struct {
+//	SystemdCGroup bool `json:"systemd_cgroup"`
+//}
 
 type Linux struct {
 	CgroupParent string `json:"cgroup_parent"`
@@ -27,8 +27,8 @@ type Config struct {
 }
 
 type sandboxInfoSpec struct {
-	Pid            int            `json:"pid"`
-	RuntimeSpec    RuntimeSpec    `json:"runtimeSpec"`
-	RuntimeOptions RuntimeOptions `json:"runtimeOptions"`
-	Config         Config         `json:"config"`
+	Pid int `json:"pid"`
+	//RuntimeSpec    RuntimeSpec    `json:"runtimeSpec"`
+	//RuntimeOptions RuntimeOptions `json:"runtimeOptions"`
+	Config Config `json:"config"`
 }
