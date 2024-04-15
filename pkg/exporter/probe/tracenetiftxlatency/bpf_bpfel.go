1b3532c7c8b334215b58ffd9c2875d83d700e048
bingshen.wbs <bingshen.wbs@alibaba-inc.com>
2024-04-15 10:35:17 +0800

    feat: support bpf prog check in ci
    Signed-off-by: bingshen.wbs <bingshen.wbs@alibaba-inc.com>

--- pkg/exporter/probe/tracenetiftxlatency/bpf_bpfel.go
+++ pkg/exporter/probe/tracenetiftxlatency/bpf_bpfel.go
@@ -17,14 +17,8 @@ type bpfInspNftxlatEventT struct {
 	Target [20]int8
 	Type   uint32
 	Tuple  struct {
-		Saddr struct {
-			V4addr uint32
-			_      [12]byte
-		}
-		Daddr struct {
-			V4addr uint32
-			_      [12]byte
-		}
+		Saddr   struct{ V6addr [16]uint8 }
+		Daddr   struct{ V6addr [16]uint8 }
 		Sport   uint16
 		Dport   uint16
 		L3Proto uint16
